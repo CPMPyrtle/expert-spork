db63ee42c726d27ba008cb3f7b2ca511f68fb464
CPMPyrtle <courtney.pyrtle28@gmail.com>
2024-01-29 14:58:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import requests
+import flask
+import json
+
+#Handle errors such as invalid genre inputs, API errors, or empty responses
+
+#Fetch a list of all movies with an API call
+
+#Filter the movies based on genre (determined by the genre parameter)
+
+#Get additional details for each movie using a separate API call
+
+#Calculate the total run time of the movies in the genre
+
+#Calculate the average rating for the movies in the genre
+
+#For each movie store:
+  #Title, Genre, Rating, Relative Rating (rating minues the avg rating in the genre), Runtime (runtime in a human friendly format)
+
+#Sort the movies based on relative rating (highest first) and trim to the first N records (set by the limit parameter)
+
+#Return all data in JSON format
